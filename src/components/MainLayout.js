a3b2c47f5c23b3dad557e9405a6f7e02d2b8a4f6
eLDiii <m4iky333111@gmail.com>
2023-02-07 20:53:26 +0100

    animations

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -3,6 +3,7 @@ import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import "mapbox-gl/dist/mapbox-gl.css";
 import * as turf from "@turf/turf";
+import { motion } from "framer-motion";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_GL_KEY;
 
@@ -11,16 +12,21 @@ export default function MainLayout() {
   const map = useRef(null);
   const turfOptions = { units: "meters" };
 
-  const [lng, setLng] = useState(18.8496);
-  const [lat, setLat] = useState(48.5918);
-  const [zoom, setZoom] = useState(15);
+  //state for map variables
+  const [lng, setLng] = useState(18.853731);
+  const [lat, setLat] = useState(48.587415);
+  const [zoom, setZoom] = useState(16);
 
+  //game related state
   const [currentObjective, setCurrentObjective] = useState(0);
+  const [nextObjective, setNextObjective] = useState(1);
   const [distance, setDistance] = useState(0);
   const [isNearObjective, setIsNearObjective] = useState(false);
 
+  //markers json
   const missions = require("../assets/markers.json");
 
+  //initialize map
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
@@ -29,14 +35,16 @@ export default function MainLayout() {
       center: [lng, lat],
       zoom: zoom,
     });
+  });
+
+  //register geolocate control
+  useEffect(() => {
     map.current.addControl(
       new mapboxgl.GeolocateControl({
         positionOptions: {
           enableHighAccuracy: true,
         },
-        // When active the map will receive updates to the device's location as it changes.
         trackUserLocation: true,
-        // Draw an arrow next to the location dot to indicate which direction the device is heading.
         showUserHeading: true,
         showAccuracyCircle: false,
       }).on("geolocate", function (e) {
@@ -46,29 +54,50 @@ export default function MainLayout() {
     );
   });
 
+  //marker for the current objective
   useEffect(() => {
-    const marker = missions[currentObjective];
-    new mapboxgl.Marker()
-      .setLngLat([marker.lon, marker.lat])
+    if (!map.current) return; // wait for map to initialize
+    const marker = new mapboxgl.Marker()
+      .setLngLat([
+        missions[currentObjective].lon,
+        missions[currentObjective].lat,
+      ])
       .addTo(map.current);
+    //remove last marker on re-render
+    return () => marker.remove();
   }, [currentObjective, missions]);
 
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
     map.current.on("move", () => {
       setZoom(map.current.getZoom().toFixed(2));
+
       let line = turf.lineString([
         [lng, lat],
         [missions[currentObjective].lon, missions[currentObjective].lat],
       ]);
       setDistance(turf.length(line, turfOptions).toFixed(0));
-      if (distance < 0.03) {
+
+      if (distance < 20) {
         // player is near the objective
-        //TODO: add a popup with a button to confirm the objective
         setIsNearObjective(true);
+      } else {
+        setIsNearObjective(false);
       }
     });
   });
+
+  //handler for button confirming the objective was delivered
+  function deliveryButtonHandler() {
+    setCurrentObjective(0);
+    console.log(currentObjective);
+  }
+
+  //handler for button confirming the next objective has been picked up
+  function nextObjectiveHandler() {
+    setCurrentObjective(nextObjective);
+    setNextObjective(nextObjective + 1);
+  }
 
   return (
     <>
@@ -78,12 +107,18 @@ export default function MainLayout() {
             ref={mapContainer}
             className="m-4 border-solid border-4 border-main_light_blue rounded-3xl z-0 relative mb-[3.6rem]"
           >
-            {
-              <button className="absolute bottom-10 left-2 z-10 bg-main_dark_blue text-white p-2 px-3 font-bold rounded-xl border-solid border-4 border-main_light_blue">
+            {isNearObjective && (
+              <motion.button
+                initial={{ y: 50, x: "-50%" }}
+                animate={{ y: [30, 15, 30] }}
+                transition={{ duration: 2, repeat: Infinity }}  
+                className="absolute bottom-10 left-1/2 -translate-x-1/2 z-20 bg-main_dark_blue text-white pb-8 px-8 pt-2 font-black rounded-xl border-solid border-4 border-main_light_blue will-change-transform"
+                onClick={deliveryButtonHandler}
+              >
                 Confirm
-              </button>
-            }
-            <div className="absolute z-10 top-4 left-2 text-sm bg-main_dark_blue text-white p-2 px-3 font-bold rounded-xl border-solid border-4 border-main_light_blue">
+              </motion.button>
+            )}
+            <div className="absolute z-10 top-4 left-2 text-xs bg-main_dark_blue text-white p-2 px-3 font-bold rounded-xl border-solid border-4 border-main_light_blue">
               {`Current Objective: ${missions[currentObjective].name} (${distance} m)`}
             </div>
           </div>
