114ac002d100d60910411975eb0f7637f0c0b980
Marek Kosznovszki <61242261+ELdiii@users.noreply.github.com>
2024-04-09 15:37:49 +0200

    area lol

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -187,7 +187,7 @@ export default function MainLayout({ logOutFunc }) {
 
     //ak sa hráč nachádza pri predmete v okruhu 20m
     //zobrazí sa dialog a možnosť predmet vziať
-    if (turf.length(line, turfOptions).toFixed(0) < 70) {
+    if (turf.length(line, turfOptions).toFixed(0) < 100) {
       setIsNearMission(true);
       return;
     }
